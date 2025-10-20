f1f2fecb52b3a2225b3429caee3c42b4fec53ceb
mhdtarras <mohammadgamer1t.v@gmail.com>
2025-10-20 23:24:34 +0300

    saving

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import DoctorRegister from "./pages/registers/doctorRegister/DoctorRegister";
 import "./App.css";
 import PatientRegister from "./pages/registers/patientRegister/PatientRegister";
 import DeliveryRegister from "./pages/registers/dileveryRegister/DileveryRegister";
+import ServicesCard from "./components/servicesCard/ServicesCard";
 export default function App() {
   return (
     <>
   
-    <DeliveryRegister/>
+    
   
      
     </>
