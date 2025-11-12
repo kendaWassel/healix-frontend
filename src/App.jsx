0774ecdec607ea4e91fa3dffe6b9fdc3f48b8221
mhdtarras <mohammadgamer1t.v@gmail.com>
2025-11-13 01:04:23 +0300

    Linking 2 pages  and edits in appointment routes and editing nurse header

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,6 @@ import Doctor from "./pages/doctor/Doctor";
 import "./App.css";
 import Nurse from "./pages/nurse/Nurse";
 import Physio from "./pages/physio/Physio";
-import Appointments from"./pages/Patient/DoctorConsultation/Appointments/Appointments";
 export default function App() {
   return (
     <>
